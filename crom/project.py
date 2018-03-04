bf6c5ee4af5c2a3825967e3581dd101c11f2017c
Mathieu Ropert <mro@puchiko.net>
2018-03-04 22:04:35 +0100

    Refactor bootstrap and generate build desc

--- crom/project.py
+++ crom/project.py
@@ -1,43 +1,6 @@
-def with_directory(file, dir):
-    if dir is None:
-        return file
-    else:
-        return'/'.join([dir, file])
-
-
-def list_files(files, dir):
-    return sorted(map(lambda f: with_directory(f, dir), files))
-
-
-def get_files(files, dir):
-    return dict(map(lambda p: (with_directory(p[0], dir), p[1]), files.items()))
-
-
 class Project:
-    def __init__(self, name, sources={}, headers={}, tests={}):
+    def __init__(self, name, sources=[], headers=[], tests=[]):
         self.name = name
-        self.sources = sources
-        self.headers = headers
-        self.tests = tests
-
-    def list_sources(self, src_dir=None):
-        return list_files(self.sources.keys(), src_dir)
-
-    def list_headers(self, include_dir=None):
-        return list_files(self.headers.keys(), include_dir)
-
-    def list_tests(self, test_dir=None):
-        return list_files(self.tests.keys(), test_dir)
-
-    def list_sources_and_headers(self, src_dir=None, include_dir=None):
-        return self.list_sources(src_dir) + self.list_headers(include_dir)
-
-    def list_all_files(self, src_dir=None, include_dir=None, test_dir=None):
-        return (self.list_sources(src_dir) + self.list_headers(include_dir)
-                + self.list_tests(test_dir))
-
-    def get_all_files(self, src_dir=None, include_dir=None, test_dir=None):
-        files = get_files(self.sources, src_dir)
-        files.update(get_files(self.headers, include_dir))
-        files.update(get_files(self.tests, test_dir))
-        return files
+        self.sources = sorted(sources)
+        self.headers = sorted(headers)
+        self.tests = sorted(tests)
--- tests/test_project.py
+++ tests/test_project.py
@@ -2,9 +2,9 @@ from crom.project import Project
 
 
 def test_constructor():
-    sources = {'bar.cpp': 'x', 'foo.cpp': 'y'}
-    headers = {'hello/bar.hpp': 'z', 'hello/foo.hpp': 'w'}
-    tests = {'test.cpp': 't'}
+    sources = ['bar.cpp', 'foo.cpp']
+    headers = ['hello/bar.hpp', 'hello/foo.hpp']
+    tests = ['test/test.cpp']
     project = Project('hello', sources=sources, headers=headers, tests=tests)
     assert project.name == 'hello'
     assert project.sources == sources
@@ -12,51 +12,11 @@ def test_constructor():
     assert project.tests == tests
 
 
-def test_list_files():
-    sources = {'bar.cpp': 'x', 'foo.cpp': 'y'}
-    headers = {'hello/bar.hpp': 'z', 'hello/foo.hpp': 'w'}
-    tests = {'test.cpp': 't'}
+def test_constructor_unsorted():
+    sources = ['foo.cpp', 'bar.cpp']
+    headers = ['hello/bazz.hpp', 'hello/foo.hpp', 'hello/bar.hpp']
+    tests = ['test/test.cpp']
     project = Project('hello', sources=sources, headers=headers, tests=tests)
-    assert project.list_sources('src') == ['src/bar.cpp', 'src/foo.cpp']
-    assert project.list_headers('include') == ['include/hello/bar.hpp', 'include/hello/foo.hpp']
-    assert project.list_tests('test') == ['test/test.cpp']
-    assert project.list_sources_and_headers('src', 'include') == (
-        project.list_sources('src') + project.list_headers('include'))
-    assert project.list_all_files('src', 'include', 'test') == (
-        project.list_sources('src') + project.list_headers('include') + project.list_tests('test'))
-
-
-def test_list_files_unsorted():
-    sources = {'foo.cpp': 'y', 'bar.cpp': 'x'}
-    headers = {'hello/bazz.hpp': 'b', 'hello/foo.hpp': 'w', 'hello/bar.hpp': 'z'}
-    tests = {'test.cpp': 't'}
-    project = Project('hello', sources=sources, headers=headers, tests=tests)
-    assert project.list_all_files('src', 'include', 'test') == [
-        'src/bar.cpp', 'src/foo.cpp',
-        'include/hello/bar.hpp', 'include/hello/bazz.hpp', 'include/hello/foo.hpp',
-        'test/test.cpp'
-    ]
-
-
-def test_list_files_with_empty_directory():
-    sources = {'bar.cpp': 'x', 'foo.cpp': 'y'}
-    headers = {'hello/bar.hpp': 'z', 'hello/foo.hpp': 'w'}
-    tests = {'test.cpp': 't'}
-    project = Project('hello', sources=sources, headers=headers, tests=tests)
-    assert project.list_all_files() == [
-        'bar.cpp', 'foo.cpp',
-        'hello/bar.hpp', 'hello/foo.hpp',
-        'test.cpp'
-    ]
-
-
-def test_get_files():
-    sources = {'bar.cpp': 'x', 'foo.cpp': 'y'}
-    headers = {'hello/bar.hpp': 'z', 'hello/foo.hpp': 'w'}
-    tests = {'test.cpp': 't'}
-    project = Project('hello', sources=sources, headers=headers, tests=tests)
-    assert project.get_all_files() == {
-        'bar.cpp': 'x', 'foo.cpp': 'y',
-        'hello/bar.hpp': 'z', 'hello/foo.hpp': 'w',
-        'test.cpp': 't'
-    }
+    assert project.sources == ['bar.cpp', 'foo.cpp']
+    assert project.headers == ['hello/bar.hpp', 'hello/bazz.hpp', 'hello/foo.hpp']
+    assert project.tests == ['test/test.cpp']
